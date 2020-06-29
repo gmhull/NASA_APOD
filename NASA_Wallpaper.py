07b773c0d86e16f4f09f34e93c702d02b69ce4ea
Garth <59071040+gmhull@users.noreply.github.com>
2020-06-29 19:25:45 -0400

    Update NASA_Wallpaper.py

--- NASA_Wallpaper.py
+++ NASA_Wallpaper.py
@@ -2,7 +2,7 @@ import datetime, requests, os
 from PIL import Image
 from io import BytesIO
 
-def get_image(base_url, dir, days):
+def get_image(base_url, dir, years):
     for file in os.listdir(dir):
         file_path = os.path.join(dir, file)
         try:
@@ -10,22 +10,25 @@ def get_image(base_url, dir, days):
                 os.unlink(file_path)
         except:
             print("File Didn't Delete")
-    for day in range(days):
-        DATE = datetime.datetime.now() - datetime.timedelta(days=day)
+    for year in range(years):
+        DATE = datetime.datetime.now() - datetime.timedelta(days=365*year)
         DATE = DATE.strftime("%Y-%m-%d")
         url = base_url + DATE
-        NASA_POD_URL = requests.get(url).json()['url']
-        print(requests.get(url).json()['explanation'])
-        img = requests.get(NASA_POD_URL)
-        img = Image.open(BytesIO(img.content))
-        img.save(dir + '/APOD_' + DATE + ".JPG")
+        if requests.get(url).json()['media_type'] == "image":
+            NASA_POD_URL = requests.get(url).json()['hdurl']
+            img = requests.get(NASA_POD_URL)
+            img = Image.open(BytesIO(img.content))
+            img.save(dir + '/APOD_' + DATE + ".JPG")
+        else:
+            print("%s did not have an image" % (DATE))
+            continue
 
 def main():
-    API_KEY = '2Yi6UDVVE4lMtSRex9pdBhPjXuhft3ylshAOvPVO'
-    DAYS = 1
-    DIRECTORY = 'C:/Users/Garth/Pictures/Wallpaper'
+    API_KEY = 'XXXXXX'
+    YEARS = 10
+    DIRECTORY = 'XXXXXX'
     NASA_API_URL = "https://api.nasa.gov/planetary/apod?api_key=%s&date=" % (API_KEY)
-    get_image(NASA_API_URL, DIRECTORY, DAYS)
+    get_image(NASA_API_URL, DIRECTORY, YEARS)
 
 if __name__ == "__main__":
     main()
